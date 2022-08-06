cb9eea6490ff21af6f0161f455c92f33820691e0
garlicbean <97859742+garlicbean@users.noreply.github.com>
2022-08-06 04:58:51 -0400

    Add files via upload

--- jpegAnalysis.py
+++ jpegAnalysis.py
@@ -1,5 +1,5 @@
 from markerDict import markerDict
-def findMarkers(binary):
+def getMarkers(binary):
 #Outputs a list of tuples (Marker ID, Index) in a binary
 
 	outList = []
@@ -15,4 +15,16 @@ def getMarkerLength(binary, findMarkersOutput):
 	[outList.append((i[0], i[1], ((binary[i[1]+2] << 2) + binary[i[1]+3]))) for i in findMarkersOutput if markerDict[i[0]][3] == 1]
 	return outList
 
+def getData(binary, index, length, flag=0):
 
+#flags:
+#0 - Returns marker bytes, length bytes, and data
+#1 - Returns length bytes and data
+#2 - Returns data
+        match flag:
+                case 0:
+                        return binary[(index):(index+length)]
+                case 1:
+                        return binary[(index+2):(index+length)]
+                case 2:
+                        return binary[(index+4):(index+length)]
