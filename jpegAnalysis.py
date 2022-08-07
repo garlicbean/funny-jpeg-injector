ca29284c611ff2b3254c1ec4e674c10a2aa5392d
garlicbean <97859742+garlicbean@users.noreply.github.com>
2022-08-07 02:50:32 -0400

    Update jpegAnalysis.py

--- jpegAnalysis.py
+++ jpegAnalysis.py
@@ -5,12 +5,7 @@ def getMarkers(binary):
 	[outList.append((markerTupleDict[(binary[x], binary[x+1])][0], x)) for x, i in enumerate(binary) if binary[x] == 255 if (binary[x], binary[x+1]) in markerTupleDict]
 	return outList
 
-def getData(binary, *index, flag=0):
-
-	#flags:
-	#0 - Returns marker bytes, length bytes, and data
-	#1 - Returns length bytes and data
-	#2 - Returns data
+def getData(binary, *index):
 
 	outList = []
 	sortedIndex = []
@@ -18,11 +13,5 @@ def getData(binary, *index, flag=0):
 	sortedIndex.sort(reverse=True)
 	for i in sortedIndex:
 		datalen = ((binary[i+2] << 8) + (binary[i+3]))
-		match flag:
-			case 0:
-				outList.append(binary[(i):(i+datalen)])
-			case 1:
-				outList.append(binary[(i+2):(i+datalen)])
-			case 2:
-				outList.append(binary[(i+4):(i+datalen)])
+			outList.append(binary[(i):(i+datalen)])
 	return outList
